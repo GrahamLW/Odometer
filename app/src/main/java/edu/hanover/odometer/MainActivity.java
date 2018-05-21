52f8b37dd7992b0cd97e5fe74d4581667a0004e6
Graham Wood <grahamwood13@gmail.com>
2018-05-20 21:17:50 -0400

    Initial commit

---
+++ app/src/main/java/edu/hanover/odometer/MainActivity.java
@@ -0,0 +1,21 @@
+package edu.hanover.odometer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Handler;
+import android.os.IBinder;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
