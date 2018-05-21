51931c2b9ca017212c70632d6dd5623a0e22d81a
Graham Wood <grahamwood13@gmail.com>
2018-05-20 21:28:15 -0400

    fixed R problem close #1

--- app/src/main/java/edu/hanover/odometer/MainActivity.java
+++ app/src/main/java/edu/hanover/odometer/MainActivity.java
@@ -11,11 +11,29 @@ import android.os.IBinder;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    private OdometerService odometer;
+    private boolean bound = false;
 
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder binder) {
+            OdometerService.OdometerBinder odometerBinder = (OdometerService.OdometerBinder) binder;
+            odometer = odometerBinder.getOdometer();
+            bound = true;
+        }
+        @Override
+        public void onServiceDisconected(ComponentName componentName) {
+            bound = false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        watchMileage();
     }
+
+    @Override
+    protected
 }
